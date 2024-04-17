85cc8b21b042217cdde79358055112c494a651cf
Shrikant Koltur <55858514+Atomic-man007@users.noreply.github.com>
2024-04-17 23:54:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def get_text_chunks(text):
 # use FAISS vector storage
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    # use chromaDB as alternative 
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
